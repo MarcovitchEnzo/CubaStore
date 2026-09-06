cd9d49b191876e1884e0eef7fac92c0292997fd7
Lucas Araujo <lucascelestinoprofissional@gmail.com>
2026-09-06 05:07:59 -0300

    Function Update

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -1,8 +1,15 @@
 import pandas as pd
+import os
 
 def ler_csv(caminho_arquivo):
-    df = pd.read_csv(caminho_arquivo)
-    return df
+    if not os.path.exists(caminho_arquivo):
+        raise FileNotFoundError("Arquivo não encontrado: " + caminho_arquivo)
+
+    try:
+        df = pd.read_csv(caminho_arquivo)
+        return df
+    except Exception:
+        raise Exception("Erro ao ler o arquivo: " + caminho_arquivo)
 
 clientes = ler_csv("../../data/raw/customers.csv")
 print(clientes)
