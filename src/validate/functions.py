1d67a3edacd0f121f651b9ae77d519d90149109e
MarcovitchEnzo <marcovitchenzo@gmail.com>
2026-09-07 05:49:47 -0300

    New Functions for Validate

---
+++ src/validate/functions.py
@@ -0,0 +1,17 @@
+#Função para verificar nulos:
+def verificar_nulos(df, nome):
+    val = df.isnull().sum().sum()
+    print(f"NULOS EM {nome}:")
+    return val
+
+#Função para verificar duplicados:
+def verificar_duplicados(df,coluna, nome):
+    val = df[coluna].duplicated().sum()
+    print(f"DUPLICADOS EM {nome}:")
+    return val
+
+#Função para verificar se os dados estão alinhados em tabelas diferentes:
+def verificar_td(df1, coluna1, nome1, df2, coluna2, nome2):
+    val = (~df1[coluna1].isin(df2[coluna2])).sum()
+    print(f"REFERÊNCIAS INVÁLIDAS EM {nome1} → {nome2}:")
+    return val
