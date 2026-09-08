b3e03939b972394aaf942638242e96fef89b98d9
MarcovitchEnzo <marcovitchenzo@gmail.com>
2026-09-08 03:50:37 -0300

    Improve rejected records validation

--- src/utils/reject_utils.py
+++ src/utils/reject_utils.py
@@ -1,14 +1,11 @@
-import pandas as pd
 import os
 
 
-def salvar_rejeitados(dados_rejeitados, caminho_arquivo, motivo):
+def salvar_rejeitados(dados_rejeitados, caminho_arquivo):
     diretorio = os.path.dirname(caminho_arquivo)
 
     if diretorio and not os.path.exists(diretorio):
         os.makedirs(diretorio)
-
-    dados_rejeitados["rejection_reason"] = motivo
 
     if os.path.exists(caminho_arquivo):
         dados_rejeitados.to_csv(caminho_arquivo, mode="a", header=False, index=False)
@@ -16,3 +13,7 @@ def salvar_rejeitados(dados_rejeitados, caminho_arquivo, motivo):
         dados_rejeitados.to_csv(caminho_arquivo, index=False)
 
     return caminho_arquivo
+
+def reject_reason(dados_rejeitados, motivo):
+    dados_rejeitados["rejection_reason"] = motivo
+    return dados_rejeitados
